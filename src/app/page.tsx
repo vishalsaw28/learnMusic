4b0eab4af5e46c89ecccb8af76bc52fff5fc5333
vishal kumar <149081237+sawvishal@users.noreply.github.com>
2025-07-23 01:21:56 +0530

    Added spotlight and the moving border to thr button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
+import { Button } from "../ui/moving-border";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]"></main>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+    </main>
   );
 }
---
+++ src/components/ui/moving-border.tsx
@@ -0,0 +1,144 @@
+"use client";
+import React from "react";
+import {
+  motion,
+  useAnimationFrame,
+  useMotionTemplate,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
+import { useRef } from "react";
+import { cn } from "@/lib/utils";
+
+export function Button({
+  borderRadius = "1.75rem",
+  children,
+  as: Component = "button",
+  containerClassName,
+  borderClassName,
+  duration,
+  className,
+  ...otherProps
+}: {
+  borderRadius?: string;
+  children: React.ReactNode;
+  as?: any;
+  containerClassName?: string;
+  borderClassName?: string;
+  duration?: number;
+  className?: string;
+  [key: string]: any;
+}) {
+  return (
+    <Component
+      className={cn(
+        "relative h-16 w-40 overflow-hidden bg-transparent p-[1px] text-xl",
+        containerClassName
+      )}
+      style={{
+        borderRadius: borderRadius,
+      }}
+      {...otherProps}
+    >
+      <div
+        className="absolute inset-0"
+        style={{ borderRadius: `calc(${borderRadius} * 0.96)` }}
+      >
+        <MovingBorder duration={duration} rx="30%" ry="30%">
+          <div
+            className={cn(
+              "h-20 w-20 bg-[radial-gradient(#0ea5e9_40%,transparent_60%)] opacity-[0.8]",
+              borderClassName
+            )}
+          />
+        </MovingBorder>
+      </div>
+
+      <div
+        className={cn(
+          "relative flex h-full w-full items-center justify-center border border-slate-800 bg-slate-900/[0.8] text-sm text-white antialiased backdrop-blur-xl",
+          className
+        )}
+        style={{
+          borderRadius: `calc(${borderRadius} * 0.96)`,
+        }}
+      >
+        {children}
+      </div>
+    </Component>
+  );
+}
+
+export const MovingBorder = ({
+  children,
+  duration = 3000,
+  rx,
+  ry,
+  ...otherProps
+}: {
+  children: React.ReactNode;
+  duration?: number;
+  rx?: string;
+  ry?: string;
+  [key: string]: any;
+}) => {
+  const pathRef = useRef<any>(1);
+  const progress = useMotionValue<number>(0);
+
+  useAnimationFrame((time) => {
+    const node = pathRef.current;
+    if (!node || typeof node.getTotalLength !== "function") return;
+
+    const length = node.getTotalLength();
+    const pxPerMillisecond = length / duration;
+    progress.set((time * pxPerMillisecond) % length);
+  });
+
+  const x = useTransform(
+    progress,
+    (val) => pathRef.current?.getPointAtLength?.(val)?.x ?? 0
+  );
+
+  const y = useTransform(
+    progress,
+    (val) => pathRef.current?.getPointAtLength?.(val)?.y ?? 0
+  );
+
+  const transform = useMotionTemplate`translateX(${x}px) translateY(${y}px) translateX(-50%) translateY(-50%)`;
+
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none"
+        className="absolute inset-0 h-full w-full"
+        width="100%"
+        height="100%"
+        {...otherProps}
+      >
+        <rect
+          fill="none"
+          width="100%"
+          height="100%"
+          rx={rx}
+          ry={ry}
+          ref={pathRef}
+        />
+      </svg>
+      <motion.div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          display: "inline-block",
+          transform,
+          // background: "radial-gradient(#0ea5e9 40%, transparent 60%)",
+          // opacity: 0.8,
+        }}
+        // className="absolute top-0 left-0 h-6 w-6 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[radial-gradient(#0ea5e9_40%,transparent_60%)] opacity-80"
+      >
+        {children}
+      </motion.div>
+    </>
+  );
+};
