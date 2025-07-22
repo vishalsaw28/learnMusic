afefbee4ca126cd8f2b0815c09ccbbd87d0025c4
vishal kumar <149081237+sawvishal@users.noreply.github.com>
2025-07-23 03:34:07 +0530

    worked eith the Sticky-Scroll-Revel library(Acertinity ui) component

---
+++ src/components/StickyScrollRevel.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { div } from "framer-motion/client";
+import React from "react";
+import { StickyScroll } from "./ui/sticky-scroll-reveal";
+
+const musicSchoolContent = [
+  {
+    title: "Discover Your Sound with Us: A Personal Journey in Music Mastery",
+    description:
+      "Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.",
+    content: (
+      <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] text-white">
+        Collaborative Editing
+      </div>
+    ),
+  },
+  {
+    title: "Discover Your Sound with Us: A Personal Journey in Music Mastery",
+    description:
+      "Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.",
+    content: (
+      <div className="flex h-full w-full items-center justify-center text-white">
+        <img
+          src="/linear.webp"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
+      </div>
+    ),
+  },
+  {
+    title: "Discover Your Sound with Us: A Personal Journey in Music Mastery",
+    description:
+      "Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.",
+    content: (
+      <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] text-white">
+        Version control
+      </div>
+    ),
+  },
+  {
+    title: "Live Feedback & Engagement",
+    description:
+      "Immerse yourself in an interactive learning experience where feedback is immediate, just like real-time changes in a collaborative project. This approach enhances your understanding and mastery of music concepts and performance techniques.",
+    content: (
+      <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] text-white">
+        Running out of content
+      </div>
+    ),
+  },
+  {
+    title: "Cutting-Edge Curriculum",
+    description:
+      "Our curriculum is continuously updated to include the latest music education trends and technologies, ensuring you’re always learning with the most current and effective methods. Say goodbye to outdated materials and welcome an education that evolves with the industry.",
+  },
+  {
+    title: "Limitless Learning Opportunities",
+    description:
+      "With our expansive resource library and dynamic course offerings, you’ll never find yourself without something new to explore. Our platform provides continuous opportunities for growth, ensuring your musical skills are always advancing.",
+  },
+];
+
+function StickyScrollRevel() {
+  return (
+    <div>
+      <StickyScroll content={musicSchoolContent} />
+    </div>
+  );
+}
+
+export default StickyScrollRevel;
