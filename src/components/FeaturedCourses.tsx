91bb8f2fd36b0b494cce9c702afaa4ff42801125
vishal kumar <149081237+sawvishal@users.noreply.github.com>
2025-07-24 23:17:31 +0530

    updated ESLint config and fixed TS types (String → string, Boolean → boolean, reduced any usage

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -9,12 +9,12 @@ import { BackgroundGradient } from "./ui/background-gradient";
 
 interface Course {
   id: number;
-  title: String;
-  slug: String;
-  description: String;
+  title: string;
+  slug: string;
+  description: string;
   price: number;
-  instructor: String;
-  isFeatured: Boolean;
+  instructor: string;
+  isFeatured: boolean;
   // image: "/courses/guitar.jpg";
 }
 
